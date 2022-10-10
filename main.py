59bb1837b8ac0859a5d5d1da94abc0393f6a0ef1
ReneFuentes26 <87821722+ReneFuentes26@users.noreply.github.com>
2022-10-09 20:57:53 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#Hector Alejandro Franco Berrios
+#Rene Ismael Fuentes Benitez
+
+import funciones
+
+user = input("Ingrese su usuario: ")
+password = input("Ingrese su contraseña: ")
+
+funciones.valuser(user)
+funciones.valpas(password)
